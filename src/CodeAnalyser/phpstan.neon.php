e547fce5f89522abf49739d91573d0d63f17b732
josealbertogomez-aircury <josealberto.gomez@aircury.com>
2025-05-15 09:16:46 +0200

    Code Manager | Fix phpstan-doctrine config load

--- src/CodeAnalyser/phpstan.neon.php
+++ src/CodeAnalyser/phpstan.neon.php
@@ -1,19 +1,23 @@
 <?php declare(strict_types=1);
 
 $possiblePaths = [
-    '../../vendor/phpstan/phpstan-doctrine/extension.neon',
-    '../../../../phpstan/phpstan-doctrine/extension.neon',
+    __DIR__ . '/../../vendor/phpstan/phpstan-doctrine/extension.neon',
+    __DIR__ . '/../../../../phpstan/phpstan-doctrine/extension.neon',
 ];
 
-$includes = array_filter($possiblePaths, static fn (string $path) => file_exists($path));
+$includes = array_map(
+    static fn(string $path) => realpath($path),
+    array_values(array_filter(
+        $possiblePaths,
+        static fn(string $path) => file_exists($path)
+    ))
+);
 
 $parameters = [
     'level' => 'max',
 ];
 
-$config = [
+return [
     'includes' => $includes,
     'parameters' => $parameters,
 ];
-
-return $config;
