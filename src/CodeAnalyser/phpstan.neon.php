bfd91e8e1ae38e7195d3e7cfbbbbf5343888dd1e
Alejandro Rey <alejandro.rey@aircury.com>
2025-08-26 10:19:59 +0200

    CodeManager | Fix phpstan doctrine extension files inclusion

--- src/CodeAnalyser/phpstan.neon.php
+++ src/CodeAnalyser/phpstan.neon.php
@@ -1,17 +1,26 @@
 <?php declare(strict_types=1);
 
-$possiblePaths = [
-    __DIR__ . '/../../vendor/phpstan/phpstan-doctrine/extension.neon',
-    __DIR__ . '/../../../../phpstan/phpstan-doctrine/extension.neon',
-];
+$extensionInstallerPath = __DIR__ . '/../../vendor/phpstan/extension-installer';
+$isExtensionInstallerInstalled = is_dir($extensionInstallerPath);
 
-$includes = array_map(
-    static fn(string $path) => realpath($path),
-    array_values(array_filter(
-        $possiblePaths,
-        static fn(string $path) => file_exists($path)
-    ))
-);
+var_dump($isExtensionInstallerInstalled);
+
+$includes = [];
+
+if (!$isExtensionInstallerInstalled) {
+    $possiblePaths = [
+        __DIR__ . '/../../vendor/phpstan/phpstan-doctrine/extension.neon',
+        __DIR__ . '/../../../../phpstan/phpstan-doctrine/extension.neon',
+    ];
+
+    $includes = array_map(
+        static fn(string $path) => realpath($path),
+        array_values(array_filter(
+            $possiblePaths,
+            static fn(string $path) => file_exists($path)
+        ))
+    );
+}
 
 $parameters = [
     'level' => 'max',
